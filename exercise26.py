895d210e7d59ab63bc7c3f3ded003f3a4d9a4287
parfonovv275-design <parfonovv275@gmail.com>
2026-05-25 18:46:41 +0000

    виконали вправу 26

---
+++ exercise26.py
@@ -0,0 +1,14 @@
+a = 19
+b = 11
+x = a>b
+print( 'a>b= ', x)
+y = b <= 10 
+print( 'b <= 10 = ', y)
+x and y 
+print( 'x and y = ', x and y)
+x or y 
+print( 'x or y = ', x or y)
+not x and y 
+print( 'not x and y = ', not x and y)
+not (x and y)
+print( 'not (x and y) = ', not (x and y))
